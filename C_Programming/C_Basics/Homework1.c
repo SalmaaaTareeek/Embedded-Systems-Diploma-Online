abf31d4d2971170afce7b294ef8f6ffd8147ad0b
Salma Tarek <31780151+SalmaaaTareeek@users.noreply.github.com>
2020-10-02 15:25:37 +0200

    Update Homework1.c
    adding all the Assignment'answers

--- C_Programming/C_Basics/Homework1.c
+++ C_Programming/C_Basics/Homework1.c
@@ -12,14 +12,89 @@
 //	 printf("C Programming");
 
 	 /*EX2*/
-	 int num;
-	 printf("Enter a integer: ");
+//	 int num;
+//	 printf("Enter a integer: ");
+//	 fflush(stdout);
+//	 fflush(stdin);
+//	 scanf("%d" , &num);
+//	 fflush(stdout);
+//	 fflush(stdin);
+//	 printf("You Entered: %d",num);
+
+	 /*EX3*/
+//	 int x,y;
+//	 printf("Enter Two Integers: ");
+//	 fflush(stdout);
+//	 fflush(stdin);
+//	 scanf("%d %d", &x,&y);
+//	 printf("sum:%d", x+y);
+
+	 /*EX4*/
+//	 float x,y;
+//	 printf("Enter two numbers");
+//	 fflush(stdout);
+//	 fflush(stdin);
+//	 scanf("%f %f",&x,&y);
+//	 fflush(stdout);
+//	 fflush(stdin);
+//	 printf("product:%f",x*y);
+
+	 /*EX5*/
+//	 char c;
+//	 printf("Enter a character:");
+//	 fflush(stdout);
+//	 fflush(stdin);
+//	 scanf("%c", &c);
+//	 fflush(stdout);
+//	 fflush(stdin);
+//	 printf("ASCII Value of %c is %d",c,c);
+
+	 /*EX6*/
+//	 float a,b,temp;
+//	 printf("Enter Value of a:");
+//	 fflush(stdout);
+//	 fflush(stdin);
+//	 scanf("%f", &a);
+//	 fflush(stdout);
+//	 fflush(stdin);
+//	 printf("Enter Value of b:");
+//	 fflush(stdout);
+//	 fflush(stdin);
+//	 scanf("%f", &b);
+//	 temp = b;
+//	 b = a;
+//	 a= temp;
+//	 printf("After swapping, value of a: %f\n",a);
+//	 printf("After swapping, value of b: %f",b);
+
+	 /* EX7 */
+	 float a,b;
+	 printf("Enter Value of a:");
 	 fflush(stdout);
 	 fflush(stdin);
-	 scanf("%d" , &num);
+	 scanf("%f", &a);
 	 fflush(stdout);
 	 fflush(stdin);
-	 printf("You Entered: %d",num);
+	 printf("Enter Value of b:");
+	 fflush(stdout);
+	 fflush(stdin);
+	 scanf("%f", &b);
+	 a = a+b;
+	 b = a-b;
+	 a = a-b;
+	 printf("After swapping, value of a: %f\n",a);
+	 printf("After swapping, value of b: %f",b);
+
+
+
+
+
+
+
+
+
+
+
 
  }
 
