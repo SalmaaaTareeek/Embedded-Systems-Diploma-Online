875c3f03c9c9be2ae08c8e968a33d2f6606f40fa
Salma Tarek <31780151+SalmaaaTareeek@users.noreply.github.com>
2020-10-02 14:46:01 +0200

    Add files via upload
    First 2 EX.

---
+++ C_Programming/C_Basics/Homework1.c
@@ -0,0 +1,25 @@
+/*
+ * main.c
+ *
+ *  Created on: Oct 2, 2020
+ *      Author: Dell
+ */
+#include<stdio.h>
+ void main()
+
+ {
+	 /* EX1 */
+//	 printf("C Programming");
+
+	 /*EX2*/
+	 int num;
+	 printf("Enter a integer: ");
+	 fflush(stdout);
+	 fflush(stdin);
+	 scanf("%d" , &num);
+	 fflush(stdout);
+	 fflush(stdin);
+	 printf("You Entered: %d",num);
+
+ }
+
