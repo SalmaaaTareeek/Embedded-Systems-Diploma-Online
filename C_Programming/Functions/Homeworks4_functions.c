d67cecd78d73c9b2c69ff9e803be495970679600
Salma Tarek <31780151+SalmaaaTareeek@users.noreply.github.com>
2020-10-28 21:45:04 +0200

    first ex. in function

---
+++ C_Programming/Functions/Homeworks4_functions.c
@@ -0,0 +1,81 @@
+#include"prototypes.h"
+#include<stdio.h>
+//EX1
+void prime(int num1, int num2 )
+
+{
+	int i ;
+	int j;
+	int result = 0;
+	printf("Enter two numbers(intervals):");
+	fflush(stdin); fflush(stdout);
+	scanf("%d %d",&num1,&num2);
+
+
+	if (num2 > num1 )
+	{
+		printf("prime numbers between %d and %d is  " , num1,num2);
+		for (i= num1+1 ; i< num2 ; i++)
+		{
+			for(j = 2 ; j< i ; j++)
+			{
+				if (i%j != 0)
+				{
+					result = i;
+
+
+				}
+				else
+				{
+					result = 0;
+
+					break;
+				}
+
+			}
+			if(result != 0)
+			{
+//				fflush(stdin); fflush(stdout);
+
+				printf("%d " , result);
+			}
+
+
+
+		}
+
+	}
+	else
+	{
+		printf("prime numbers between %d and %d is  " , num2,num1);
+				for (i= num2+1 ; i< num1 ; i++)
+				{
+					for(j = 2 ; j< i ; j++)
+					{
+						if (i%j != 0)
+						{
+							result = i;
+
+
+						}
+						else
+						{
+							result = 0;
+
+							break;
+						}
+
+					}
+					if(result != 0)
+					{
+
+						printf("%d " , result);
+					}
+
+
+	}
+	}
+
+
+
+}
---
+++ C_Programming/Functions/Homeworks4_main.c
@@ -0,0 +1,19 @@
+/*
+ * main.c
+
+ *
+ *  Created on: Oct 28, 2020
+ *      Author: Dell
+ */
+#include<stdio.h>
+#include"prototypes.h"
+
+void main()
+{
+	int num1;
+	int num2;
+	prime(num1,num2);
+
+}
+
+
