521e95419a4ac763c21452c637791db4a2a15600
Salma Tarek <31780151+SalmaaaTareeek@users.noreply.github.com>
2020-10-30 18:29:19 +0200

    complete

--- C_Programming/Functions/Homeworks4_main.c
+++ C_Programming/Functions/Homeworks4_main.c
@@ -8,17 +8,42 @@
 #include<stdio.h>
 #include"prototypes.h"
 
+/* if you want to see any answer of any excersices you can uncomment the desired lines*/
+
 void main()
 {
 	int num1;
 	int num2;
 	int result;
-	//prime(num1,num2);   // EX1
-	//EX2//
+
+	// EX1
+	//prime(num1,num2);
+
+//	//EX2//
+//	scanf("%d",&num1);
+//	result = factorial(num1);
+//	printf("Factorial of %d is %d",num1 , result);
+//	//////
+
+	//EX3//
+//	printf("Enter Sentence: ");
+//	fflush(stdin); fflush(stdout);
+//	swap();
+//	///////
+
+	//EX4//
+	printf("Enter base number");
+	fflush(stdin); fflush(stdout);
 	scanf("%d",&num1);
-	result = factorial(num1);
-	printf("Factorial of %d is %d",num1 , result);
-	//////
+	printf("Enter power number");
+	fflush(stdin); fflush(stdout);
+	scanf("%d",&num2);
+	result=power(num1,num2);
+	printf("%d",result);
+	//////////////////
+
+
+
 
 }
 
