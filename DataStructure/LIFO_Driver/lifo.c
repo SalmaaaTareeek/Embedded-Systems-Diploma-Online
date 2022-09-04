e8c2a34f81ad2336afb02972e67b44ce07494438
SalmaaaTareeek <salmaaatareeek97@gmail.com>
2022-09-04 13:10:17 -0700

    Starting with LIFO Init Function

--- DataStructure/LIFO_Driver/lifo.c
+++ DataStructure/LIFO_Driver/lifo.c
@@ -2,7 +2,22 @@
  * lifo.c
  *
  *  Created on: Sep 3, 2022
- *      Author: dell
+ *      Author: Salma Tarek
  */
+#include"stdlib.h"
+#include"platform.h"
+#include"lifo.h"
+
+//Starting to Init LIFO
+Lifo_Status Lifo_init(SBuff_t * lifo , Element_Width * Buf,unsigned int length )
+{
+
+
+}
+Lifo_Status Lifo_Is_Empty(SBuff_t * lifo);
+Lifo_Status Lifo_Is_Full(SBuff_t * lifo);
+
+
+
 
 
--- DataStructure/LIFO_Driver/lifo.h
+++ DataStructure/LIFO_Driver/lifo.h
@@ -2,21 +2,43 @@
  * lifo.h
  *
  *  Created on: Sep 3, 2022
- *      Author: dell
+ *      Author: Salma Tarek
+ *      Driver : Lifo.h
  */
 
 #ifndef LIFO_H_
 #define LIFO_H_
-#include"stdlib.h"
 #include"stdio.h"
 #include"platform.h"
 
 
 //Defining User-Configuration Width
-#define Element_Width uint8_t
-
+#define Element_Width unsigned char
 //Define Size of Buffer
 #define MAX 10
+
+//Defining Structure Contains LIFO Pramaters
+typedef struct {
+	unsigned int size;
+	unsigned int count;
+	Element_Width* head;
+	Element_Width* tail;
+}SBuff_t;
+
+//Defining Status of LIFO
+typedef enum { Lifo_no_error ,
+	Lifo_full,
+	Lifo_empty,
+	Lifo_Null
+
+}Lifo_Status;
+
+//Prototype for User APIs
+
+Lifo_Status Lifo_Is_Empty(SBuff_t * lifo);
+Lifo_Status Lifo_Is_Full(SBuff_t * lifo);
+Lifo_Status Lifo_init(SBuff_t * lifo , Element_Width * Buf,unsigned int length );
+
 
 
 
